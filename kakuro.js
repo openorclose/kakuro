a1027c58170c5292d4b2333043288fc3ce4d7dff
LeowWB <41581512+LeowWB@users.noreply.github.com>
2018-07-26 20:51:27 +0800

    added a block of comments and a semicolon
    hi can you add some comments to explain what's going on? otherwise very hard for a newcomer (newcomer to js, to kakuro, and to the program) to find bugs etc. thanks

--- kakuro.js
+++ kakuro.js
@@ -3,11 +3,20 @@ Test on kakuros.com
 let sol = "2|6|3|2|1|2|4|1|2|3|5|6|3|1|9|3|8|2|1|9|3|1|5|2|8|1|7|9|3|5|4|8|1|6|5|3|9|1|6|9|8|4|9|4|5|6|8|1|6|1|9|3|8|1|7|9|8|6|3|8|4|2|9|1|9|2|3|8|2|6|1|7|8|8|9|3|6|1|9|2|2|8|1|9|1|8|1|9|8|3|9|7|9|1|9|6|8|1|7|5|1|2|6|3|5|7|9|7|1|9|2|3|7|3|8|9|2|1|3|9|1|1|8|9|3|5|1|5|7|8|2|3|2|1|1|9|3|6|1|2|9|2|6|1|7|4|3|9|7|2|1|3|7|9|8|7|4|7|5|1|8|3|9|7|6|8|7|5|3|2|9|1|9|3|1|1|4|9|5|8|5|9|8|8|1|2|4|3|6|2|7|9|1|6|3|1|2|9|2|7|8|9|7|3|4|9|4|9|7|8|9|2|3|6|9|4|8|1|5|9|7|2|4|6|8|9|6|8|1|2|8|6|5|9|9|8|2|7|3|1|2|3|7|8|9|5|8|9|3|2|7|8|6|1|4|1|5|9|1|7|2|5|9|4|1|9|2|3|7|9|4|5|3|2|6|7|2|7|1|1|6|1|2|8|1|3|1|2|8|1|3|7|3|9|9|8|5|4|1|7|6|2|8|3|4|9|1|4|9|7|1|3|8|7|9|6|2|2|4|1|2|1|6|3|1|6|2|3|1|9|7|4|9|3|9|4|8|7|2|9|2|3|1|2|8|7|1|2|3|2|5|6|3|1|9|7|1|7|4|5|1|3|8|9|6|1|9|7|1|8|7|2|7|8|9|9|1|8|4|9|3|5|1|9|8|9|1|5|9|5|1|3|9|7|6|7|1|8|6|2|7|9|2|7|1|6|4|9|1|7|1|3|9|8|9|3|1|8|6|9|2|2|9|3|7|2|4|9|3|2|2|9|2|4|7|2|9|7|1|3|4|4|8|3|1|9|1|1|8|1|2|8|7|8|2|4|1|3|7|1|8|5|3|1".split("|");
 Array.from(document.getElementsByTagName("td")).filter(e=>e.style.backgroundColor === "rgb(255, 255, 255)").forEach((x,i)=>{x.onclick();kinput(sol[i])})
  */
+
 let Type = {
     clue: Symbol("clue"),
     unused: Symbol("unused"),
     number: Symbol("number"),
 };
+
+/*
+    b   Black square on grid
+    n   Number goes here (white square on grid)
+    r   the integer following this represents clue pointing to the Right
+    d   the integer following this represents clue pointing Down
+    []  contains two clues which share the same square on the grid.
+*/
 let grid2 = [
     "bd3d4bbbbd15d3",
     "r4nnd16d6br3nn",
@@ -71,7 +80,7 @@ let getPossibleCombinations = (() => {
 
     return (sum, x) => recur(sum, x, rangeTo[Math.min(9, sum)]);
 })();
-console.log(getPossibleCombinations(45, 9))
+console.log(getPossibleCombinations(45, 9));
 let clueCells = [];
 let numberCells = [];
 
